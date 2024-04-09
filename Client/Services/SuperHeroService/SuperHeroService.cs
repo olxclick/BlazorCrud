7c6a3b4017f0f146712ee1fd5f308fc36493aacd
João Lázaro <71394672+olxclick@users.noreply.github.com>
2024-04-09 15:21:31 +0100

    Blazor Crud done

---
+++ Client/Services/SuperHeroService/SuperHeroService.cs
@@ -0,0 +1,72 @@
+﻿using BlazorApp.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Net.Http.Json;
+
+namespace BlazorApp.Client.Services.SuperHeroService
+{
+    public class SuperHeroService : ISuperHeroService
+    {
+        private readonly HttpClient _http;
+
+        private readonly NavigationManager _navigationManager;
+
+        public List<SuperHero> Heroes { get; set; } = new List<SuperHero>();
+
+        public List<Comic> Comics { get; set; } =   new List<Comic>();
+
+        public SuperHeroService(HttpClient http, NavigationManager navigationManager)
+        {
+            _http = http;
+            _navigationManager = navigationManager;
+        }
+     
+        public async Task GetComics()
+        {
+            var results = await _http.GetFromJsonAsync<List<Comic>>("api/superhero/comics");
+            Comics = results;
+        }
+
+        public async Task<SuperHero> GetSingleHero(int id)
+        {
+            var results = await _http.GetFromJsonAsync<SuperHero>($"api/superhero/{id}");
+            if (results == null)
+                throw new Exception("Hero not found");
+            return results;
+        }
+
+        public async Task GetSuperHeroes()
+        {
+            var results = await _http.GetFromJsonAsync<List<SuperHero>>("api/superhero");
+            Heroes = results;
+        }
+
+        public async Task CreateHero(SuperHero entity)
+        {
+            var result = await _http.PostAsJsonAsync("api/superhero", entity);
+
+            await SetHeroes(result);
+        }
+
+        public async Task UpdateHero(SuperHero entity)
+        {
+            var result = await _http.PutAsJsonAsync($"api/superhero/{entity.Id}", entity);
+            await SetHeroes(result);
+        }
+
+        private async Task SetHeroes(HttpResponseMessage result)
+        {
+            var response = await result.Content.ReadFromJsonAsync<List<SuperHero>>();
+            Heroes = response;
+
+            _navigationManager.NavigateTo("superheroes");
+        }
+
+        public async Task DeleteHero(int id)
+        {
+            var result = await _http.DeleteAsync($"api/superhero/{id}");
+            //var response = await result.Content.ReadFromJsonAsync<List<SuperHero>>();
+            await SetHeroes(result);
+        }
+    }
+}
---
+++ Server/Controllers/SuperHeroController.cs
@@ -0,0 +1,95 @@
+﻿using BlazorApp.Client.Pages;
+using BlazorApp.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
+        {
+            var heroes = await _context.SuperHeroes.Include(sh => sh.Comic).ToListAsync();
+            return Ok(heroes);
+        }
+
+        [HttpGet("comics")]
+        public async Task<ActionResult<List<Comic>>> GetComics()
+        {
+            var comics = await _context.Comics.ToListAsync();
+            return Ok(comics);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> GetSingleHeroes(int id)
+        {
+            var hero =  await _context.SuperHeroes
+                            .Include(h => h.Comic)
+                            .FirstOrDefaultAsync(heroes => heroes.Id == id);
+            if (hero == null)
+                return NotFound("No hero was found with the given id");
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<SuperHero>> CreateSuperhero(SuperHero hero)
+        {
+            hero.Comic = null;
+            _context.SuperHeroes.Add(hero);
+            await _context.SaveChangesAsync();
+
+            return Ok(await GetDbHeroes());
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<SuperHero>> UpdateSuperhero(SuperHero hero, int id)
+        {
+            var dbHero = await _context.SuperHeroes
+                .Include(sh => sh.Comic)
+                .FirstOrDefaultAsync(sh => sh.Id == id);
+
+            if (dbHero == null)
+                return NotFound("No hero found");
+
+            dbHero.FirstName = hero.FirstName;
+            dbHero.LastName = hero.LastName;
+            dbHero.HeroName = hero.HeroName;
+            dbHero.ComicId = hero.ComicId;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await GetDbHeroes());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<SuperHero>> DeleteSuperhero(int id)
+        {
+            var dbHero = await _context.SuperHeroes
+                .Include(sh => sh.Comic)
+                .FirstOrDefaultAsync(sh => sh.Id == id);
+            if (dbHero == null)
+                return NotFound("No hero found");
+
+            _context.SuperHeroes.Remove(dbHero);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await GetDbHeroes());
+        }
+
+        private async Task<List<SuperHero>> GetDbHeroes()
+        {
+            return await _context.SuperHeroes.Include(sh => sh.Comic).ToListAsync();
+        }
+    }
+}
